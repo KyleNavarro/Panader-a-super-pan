d4d139356018f7120dc3530acae054fe17163ddd
KyleNavarro <kyle.y.navarro.j@gmail.com>
2025-09-23 20:29:02 -0500

    vanzamos en el layaut de la pagina agregando cards de las direcciones

--- super-pan/src/App.jsx
+++ super-pan/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
               </a>
             </li>
             <li>
-              <a href={vacio}>Nuestras sedes</a>
+              <a href="#sedes">Nuestras sedes</a>
             </li>
             <li>
               <a href="https://wa.me/573043564668" target="_blank">
@@ -54,6 +54,106 @@ function App() {
           </h1>
         </div>
       </div>
+      <main>
+        <div className="cardContainer">
+          <h2>Nuestras sedes</h2>
+          <section className="cards" id="sedes">
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio Porvenir</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio Coaviconsa</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio San Anto. Carrizal</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio campo hermoso</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio villas don juan</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio oasis</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+            <div className="card">
+              <img src="/src/assets/fotico.jpg" alt="" />
+              <h3>Barrio cuaviconsa</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nobis
+              </p>
+              <button>Ver Mas</button>
+            </div>
+          </section>
+        </div>
+
+        <footer>
+          <ul>
+            <li>
+              <a href="https://wa.me/573043564668" target="_blank">
+                3043564668
+              </a>
+            </li>
+            <li>correo@correo.com</li>
+            <li>
+              <a
+                href="https://www.instagram.com/superpan_bucaramanga/"
+                target="_blank"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="#sedes">Nuestras sedes</a>
+            </li>
+          </ul>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/kyle-navarro-geek/"
+              target="_blank"
+            >
+              Elaborado por: Kyle Navarro
+            </a>
+          </span>
+        </footer>
+      </main>
     </>
   );
 }
